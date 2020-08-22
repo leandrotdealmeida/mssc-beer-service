22e4559eba1898f3cb20039b3a6fe0c467771bb2
Leandro Almeida <leandrotrovilho@gmail.com>
2020-08-22 16:06:53 -0300

    Addiding web data model. Closes #2

---
+++ src/main/java/com/japa/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,28 @@
+package com.japa.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+    private BeerStyleEnum styleEnum;
+
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/japa/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.japa.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PISLSNER, STOUT, GOSE, PORTER, IPA, ALE, WHEAT, PALE_ALE, SAISON;
+}
